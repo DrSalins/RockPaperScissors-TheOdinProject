68f06efbe34c2599406e7f015d8bfffae7b33d68
DrSalins <siddarthmudaliar93@gmail.com>
2025-08-24 12:37:40 -0400

    Updates

--- game.js
+++ game.js
@@ -1,11 +1,6 @@
-const playGameBtn = document.querySelector(".PlayRoundBtn");
-const compuScoreText = document.querySelector(".compuScore");
-const playerScoreText = document.querySelector(".plyrScore");
-
 function startGame() {
   let humanScore = 0;
   let computerScore = 0;
-
   let getComputerChoice = () => {
     let result = Math.floor(Math.random() * 3);
     if (result == 0) {
@@ -16,13 +11,13 @@ function startGame() {
       return "paper";
     }
   };
+  console.log(getComputerChoice());
 
   let getHumanChoice = () => {
-    let hChoice = prompt(
-      "What is your choice (rock, paper, or scissors)?"
-    ).toLowerCase();
+    let hChoice = prompt("What is your choice:").toLowerCase();
     return hChoice;
   };
+  console.log(getHumanChoice());
 
   let playGame = () => {
     let playRound = (humanChoice, computerChoice) => {
@@ -34,13 +29,14 @@ function startGame() {
         (humanChoice == "scissors" && computerChoice == "paper")
       ) {
         humanScore++;
+        playerScoreText.textContent = humanScore;
         return "You win! " + humanChoice + " beats " + computerChoice;
       } else {
         computerScore++;
+        compuScoreText.textContent = computerScore;
         return "You lose! " + computerChoice + " beats " + humanChoice;
       }
     };
-
     for (let i = 0; i < 5; i++) {
       let humanChoice = getHumanChoice();
       let computerChoice = getComputerChoice();
@@ -48,21 +44,15 @@ function startGame() {
       console.log(
         `Your Score:${humanScore} ===== Computer's Score: ${computerScore}`
       );
-
-      playerScoreText.textContent = humanScore;
-      compuScoreText.textContent = computerScore;
     }
-
     if (humanScore > computerScore) {
-      console.log(`You have won the game! Score:${humanScore}`);
+      return `You have won the game! Score:${humanScore}`;
     } else if (computerScore > humanScore) {
-      console.log(`Computer wins the game! Score:${computerScore}`);
+      return `Computer wins the game! Score:${computerScore}`;
     } else {
-      console.log(`It's a tie!`);
+      return `It's a tie!`;
     }
   };
 
-  playGame();
+  console.log(playGame());
 }
-
-playGameBtn.addEventListener("click", startGame);
